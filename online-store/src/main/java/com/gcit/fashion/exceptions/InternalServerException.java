9cb4071e281c10df454e4d7ad08c06e7e948a0cc
santiago <SanSan0037@gmail.com>
2018-10-08 13:30:40 -0400

    Added new exceptions

---
+++ online-store/src/main/java/com/gcit/fashion/exceptions/InternalServerException.java
@@ -0,0 +1,12 @@
+package com.gcit.fashion.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class InternalServerException extends RuntimeException {
+    public InternalServerException(String exception) {
+        super(exception);
+    }
+}
